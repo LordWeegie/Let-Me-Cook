76e2ca4ee351507d15cf530dadd1958b30404a94
Mr Coconut <bradley@sellers-blais.com>
2026-03-07 08:30:02 -0800

    Removed commented code

--- scripts/playerchef.gd
+++ scripts/playerchef.gd
@@ -6,20 +6,14 @@ func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	velocity = velocity.normalized() * speed
+	# Flip player as they move
 	if velocity.y > 0 or velocity.x > 0 or velocity.y < 0 or velocity.x < 0:
 		rotation = atan2(velocity.x, velocity.y)
 func _physics_process(delta):
+	# Animation logic
 	if velocity.y != 0 or velocity.x != 0:
 		$AnimatedSprite2D.play("walk")
 	else:
 		$AnimatedSprite2D.stop()
-#	if velocity.x > 0:
-#		global_rotation = 90
-#	if velocity.x < 0:
-#		global_rotation = -90
-#	if velocity.y > 0:
-#		global_rotation = 180
-#	if velocity.y < 0:
-#		global_rotation = -180
 	get_input()
 	move_and_slide()

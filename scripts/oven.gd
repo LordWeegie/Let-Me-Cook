b078aa5113c0798b2074331c00d40908d0362bb7
Mr Coconut <bradley@sellers-blais.com>
2026-03-09 12:01:43 -0700

    Added scene for player baked in oven

--- scripts/oven.gd
+++ scripts/oven.gd
@@ -29,4 +29,4 @@ func _on_area_2d_2_body_exited(body: Node2D) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		get_tree().reload_current_scene()
+		get_tree().change_scene_to_file("res://scenes/player_baked.tscn")

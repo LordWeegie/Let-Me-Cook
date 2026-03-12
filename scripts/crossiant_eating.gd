bf8979b1a13bfff5eacbb2147510567f6bd54677
Mr Coconut <bradley@sellers-blais.com>
2026-03-12 11:36:31 -0700

    Croissant level added

--- scripts/crossiant_eating.gd
+++ scripts/crossiant_eating.gd
@@ -14,3 +14,8 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		get_tree().reload_current_scene()
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://scenes/main.tscn")

7fe61f64fac8ef27e60a1d12aa42c6b7496b836f
Mr Coconut <bradley@sellers-blais.com>
2026-03-17 08:37:18 -0700

    Bug fix

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	Global._reset_food()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

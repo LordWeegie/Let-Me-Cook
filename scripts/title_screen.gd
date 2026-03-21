5590d6e094e885e8206a8a77c994348f0f3af49a
Mr Coconut <bradley@sellers-blais.com>
2026-03-21 09:32:03 -0700

    Likely final push!

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -22,3 +22,11 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_3_pressed() -> void:
 	Global._reset_food()
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
+
+func _on_button_5_pressed() -> void:
+	$Theperfect.play()
